2f9c43e36b3d19e1c6363ef5c0a5904d01414993
kakashaUbivasha <125974058+kakashaUbivasha@users.noreply.github.com>
2024-05-31 14:44:50 +0500

    Add files via upload

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,20 +2,13 @@ import {Link, useNavigate} from "react-router-dom";
 import Button from "../customInt/Button.jsx";
 import {useContext, useEffect} from "react";
 import {Context} from "../main.jsx";
+import HomeItems from "../components/HomeItems.jsx";
 function Home(){
     const navigate = useNavigate();
     const {allInfo} =useContext(Context)
     useEffect(()=>{
         allInfo.loadCurrentUser()
     })
-    function handleCreate(){
-        if(localStorage.getItem('token')){
-            navigate('/account')
-        }
-        else{
-            navigate('/autorization')
-        }
-    }
     return(
         <div className="mt-52 flex flex-col items-center text-center w-[1200px] m-auto">
             <h1 className="font-medium text-5xl">ItemVault</h1>
@@ -24,6 +17,7 @@ function Home(){
             <p className="mt-5">Независимо от того, являетесь ли вы коллекционером-энтузиастом или просто любите сохранять важные для вас вещи, наше веб-приложение для создания коллекций станет вашим надежным помощником в организации и управлении вашими увлечениями и интересами. Давайте вместе создадим уникальные и вдохновляющие коллекции</p>
             {/*<Button onClick={handleCreate} additionalClasses="text-center mb-3">Создать коллекцию</Button>*/}
            <Link to="/account"> <Button additionalClasses="text-center mb-3">Создать коллекцию</Button></Link>
+            <HomeItems/>
         </div>
     )
 }
